c597a756fdd16176b8c3e55af826e80211ea95ad
Sushant Gautam <susant.gautam@gmail.com>
2016-06-13 07:46:24 +0545

    webview handler added

--- app/src/main/java/madhushala/npbazzar/MainActivity.java
+++ app/src/main/java/madhushala/npbazzar/MainActivity.java
@@ -1,5 +1,7 @@
 package madhushala.npbazzar;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -19,22 +22,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         webView = (WebView) findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://www.google.com");
+        webView.setWebViewClient(new CustomWebViewClient());
+        //webView.getSettings().setJavaScriptEnabled(true);
+        //webView.loadUrl("file:///android_asset/home.html");
         //end of onCreateBundle
+
+        webView.getSettings().setAppCacheMaxSize( 5 * 1024 * 1024 ); // 5MB
+        webView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setJavaScriptEnabled( true );
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT); // load online by default
+
+        if ( !isNetworkAvailable() ) { // loading offline
+            webView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK );
+        }
+
+        webView.loadUrl( "sushant.info.np" );
+
                                     }
 
     @Override
@@ -59,5 +65,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+    //implemeted from http://stackoverflow.com/questions/14670638/webview-load-website-when-online-load-local-file-when-offline
 
-}
+    //web view client implementation
+    private class CustomWebViewClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+
+            //do whatever you want with the url that is clicked inside the webview.
+            //for example tell the webview to load that url.
+            view.loadUrl(url);
+            //return true if this method handled the link event
+            //or false otherwise
+            return true;
+        }
+    }
+            }
