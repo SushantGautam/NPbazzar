615b0306158b03426760c9bd513d73f3243c0068
Sushant Gautam <susant.gautam@gmail.com>
2016-06-17 07:56:16 +0545

    webview handler added

--- app/src/main/java/madhushala/npbazzar/MainActivity.java
+++ app/src/main/java/madhushala/npbazzar/MainActivity.java
@@ -1,18 +1,26 @@
 package madhushala.npbazzar;
 
+import android.app.Activity;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.webview);
@@ -28,20 +38,36 @@ public class MainActivity extends AppCompatActivity {
         //webView.loadUrl("file:///android_asset/home.html");
         //end of onCreateBundle
 
-        webView.getSettings().setAppCacheMaxSize( 5 * 1024 * 1024 ); // 5MB
-        webView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
+        webView.getSettings().setAppCacheMaxSize(5 * 1024 * 1024); // 5MB
+        webView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
         webView.getSettings().setAllowFileAccess(true);
         webView.getSettings().setAppCacheEnabled(true);
-        webView.getSettings().setJavaScriptEnabled( true );
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT); // load online by default
+
+
+        final Activity activity = this;
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000);
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                //webView.loadUrl("file:///android_asset/home.html");
+            }
+        });
 
         if ( !isNetworkAvailable() ) { // loading offline
             webView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK );
         }
 
-        webView.loadUrl( "sushant.info.np" );
+        webView.loadUrl("http://sushant.info.np");
 
-                                    }
+                                  }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
     private class CustomWebViewClient extends WebViewClient {
         public boolean shouldOverrideUrlLoading(WebView view, String url)
         {
-
             //do whatever you want with the url that is clicked inside the webview.
             //for example tell the webview to load that url.
             view.loadUrl(url);
@@ -84,5 +109,13 @@ public class MainActivity extends AppCompatActivity {
             //or false otherwise
             return true;
         }
+
+        //public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            //webView.loadUrl("file:///assets/home.html");
+
     }
-            }
+
+
+
+}
+
